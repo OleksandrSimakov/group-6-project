8384879065e89c4c796f2784c2f4bfdf8701cd22
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-11-04 21:50:37 +0200

    хедер без лого

---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const HeaderSection = styled.header`
+display: flex;
+justify-content: space-between;
+height: 56px;
+// background-color: pink;
+`;
+
+export const Logo = styled.svg`
+width: 90px;
+height: 31px;
+margin-top: 12px;
+margin-bottom: 13px;
+margin-left: 9px;
+`;
