2c8bd5965e0f95ef709adebd7dbb0a7b3f5b05c7
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-11-05 20:57:37 +0200

    adds redux, summary component, balance view, api service and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import { Suspense } from 'react'
+import { Suspense, lazy } from 'react'
 import { Switch } from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute'
+import routes from './routes'
+
+const BalanceView = lazy(() =>
+  import('./views/BalanceView' /*webpackChunkName: "balance-view" */),
+)
 
 export default function App() {
   return (
     <>
       <Switch>
         <Suspense fallback={<p>Загружаем...</p>}></Suspense>
+        <PrivateRoute
+          path={routes.balance}
+          component={BalanceView}
+          redirectTo={routes.home}
+        />
       </Switch>
     </>
   )
---
+++ src/redux/store.js
@@ -0,0 +1,41 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import storage from 'redux-persist/lib/storage'
+import { authReducer } from './auth'
+import { balance } from 'redux/transactions'
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+}
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+]
+
+const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    balance,
+  },
+  middleware,
+  devTools: true,
+})
+
+const persistor = persistStore(store)
+export { store, persistor }
