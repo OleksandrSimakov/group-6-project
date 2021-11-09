968ab03f3c4d3de3223c4486853d0a625859e41c
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-11-09 20:56:05 +0200

    Update auth-api.js

--- src/services/auth-api.js
+++ src/services/auth-api.js
@@ -1,36 +1,36 @@
-import axios from 'axios';
+import axios from 'axios'
 
-axios.defaults.baseURL = 'https://kapusta-pro.herokuapp.com/';
+axios.defaults.baseURL = 'https://back-group-6-test.herokuapp.com/'
 
 export const token = {
   set(token) {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`
   },
   unset() {
-    axios.defaults.headers.common.Authorization = '';
+    axios.defaults.headers.common.Authorization = ''
   },
-};
+}
 
 export async function postSignUp(newUser) {
-  const { data } = await axios.post(`users/signup`, newUser);
-  console.log(`data`, data);
-  token.set(data.token);
-  return data;
+  const { data } = await axios.post(`users/signup`, newUser)
+  console.log(`data`, data)
+  token.set(data.token)
+  return data
 }
 
 export async function postLogIn(user) {
-  const { data } = await axios.post(`users/login`, user);
-  token.set(data.token);
-  return data;
+  const { data } = await axios.post(`users/login`, user)
+  token.set(data.token)
+  return data
 }
 
 export async function postLogOut() {
-  const { data } = await axios.post(`users/logout`);
-  token.unset();
-  return data;
+  const { data } = await axios.post(`users/logout`)
+  token.unset()
+  return data
 }
 
 export async function getCurrentUser() {
-  const { data } = await axios.get(`users/current`);
-  return data;
+  const { data } = await axios.get(`users/current`)
+  return data
 }
