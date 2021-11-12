862e8d9d24ca40004b27b86e83ba9eda6f80b92e
AlexeyGrk <alexgmirko@gmail.com>
2021-11-12 11:23:12 +0200

    add double accept for exit

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useHistory } from 'react-router'
-import { authOperations, authSelectors } from '../../redux/auth'
-import defaultAvatar from '../../images/default-avatar.png'
-import { IoLogOutOutline } from 'react-icons/io5'
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { authOperations, authSelectors } from "../../redux/auth";
+import defaultAvatar from "../../images/default-avatar.png";
+import { IoLogOutOutline } from "react-icons/io5";
 import {
   UserImg,
   UserName,
@@ -11,30 +11,38 @@ import {
   VerticalLine,
   MobileExitButton,
   ExitButton,
-} from './UserMenu.styled'
+} from "./UserMenu.styled";
+import { AcceptModalComponent } from "../Modal/AcceptModal/AcceptModalComponent";
 
 export default function UserMenu() {
-  const dispatch = useDispatch()
-  const userEmail = useSelector(authSelectors.getUserEmail)
-  const avatar = defaultAvatar
-  const history = useHistory()
+  const dispatch = useDispatch();
+  const userEmail = useSelector(authSelectors.getUserEmail);
+  const avatar = defaultAvatar;
+  const history = useHistory();
 
   const handleLogOut = () => {
-    dispatch(authOperations.logOut())
-    history.push('/login')
-  }
-
+    dispatch(authOperations.logOut());
+    history.push("/login");
+  };
+  const [show, setShow] = useState(false);
   return (
     <UserMenuContainer>
       <UserImg src={avatar} alt="" width="32" />
       <UserName>{userEmail}</UserName>
       <VerticalLine />
-      <ExitButton type="button" onClick={() => handleLogOut()}>
+      <ExitButton type="button" onClick={() => setShow(!show)}>
         Выйти
       </ExitButton>
       <MobileExitButton type="button" onClick={() => handleLogOut()}>
         <IoLogOutOutline color="#CBCCD0" />
       </MobileExitButton>
+      <AcceptModalComponent
+        show={show}
+        setShow={setShow}
+        handleLogOut={handleLogOut}
+      >
+        Вы уверены что хотите выйти?
+      </AcceptModalComponent>
     </UserMenuContainer>
-  )
+  );
 }
