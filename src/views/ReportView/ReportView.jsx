14681e1936901dbe195713913c779e87be926fdb
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-11-16 21:57:16 +0200

    completes SummaryByCategory in ReportView render

--- src/views/ReportView/ReportView.jsx
+++ src/views/ReportView/ReportView.jsx
@@ -1,83 +1,92 @@
-import React, { lazy, useEffect, useState } from "react";
-import WindowDimensions from '../../hooks/useWindowDimensions';
-import s from './ReportView.module.css';
-import { useRouteMatch, useLocation, } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import React, { lazy, useEffect, useState } from 'react'
+import WindowDimensions from '../../hooks/useWindowDimensions'
+import s from './ReportView.module.css'
 
-import reportOperations from '../../redux/report/report-operations';
-import reportSelectors from '../../redux/report/report-selectors';
+import { useDispatch, useSelector } from 'react-redux'
+import SummaryByCategory from '../../components/SummaryByCategory/SummaryByCategory'
 
+import reportOperations from '../../redux/report/report-operations'
+import reportSelectors from '../../redux/report/report-selectors'
 
-const Balance = lazy(() => import('../../components/Balance/Balance'));
-const ChartComponent = lazy(() => import('../../components/ChartsComponent/ChartComponent'));
-const CurrentMonth = lazy(() => import('../../components/CurrentMonth/CurrentMonth'));
-const CurrentAmount = lazy(() => import('../../components/CurrentAmount/CurrentAmount'));
-const ArrowGoBack = lazy(() => import('../../components/ArrowGoBack/ArrowGoBack'));
-const OnHandleCategories = lazy(() => import('../../components/OnHandleCategories/OnHandleCategories'));
-//const CategoriesList = lazy(() => import('../../components/CategoriesList/CategoriesList'));
-
-
+const Balance = lazy(() => import('../../components/Balance/Balance'))
+// const ChartComponent = lazy(() =>
+//   import('../../components/ChartsComponent/ChartComponent'),
+// )
+const CurrentMonth = lazy(() =>
+  import('../../components/CurrentMonth/CurrentMonth'),
+)
+const CurrentAmount = lazy(() =>
+  import('../../components/CurrentAmount/CurrentAmount'),
+)
+const ArrowGoBack = lazy(() =>
+  import('../../components/ArrowGoBack/ArrowGoBack'),
+)
+const OnHandleCategories = lazy(() =>
+  import('../../components/OnHandleCategories/OnHandleCategories'),
+)
 
 const ReportView = () => {
-  const window = WindowDimensions();
-  const dispatch = useDispatch();
+  const window = WindowDimensions()
+  const dispatch = useDispatch()
 
-  let date = new Date();
-  const [month, setMonth] = useState(date.getMonth() + 1);
-  const [year, setYear] = useState(date.getFullYear());
-  const [category, setCategory] = useState('');
-  const [type, setType] = useState('expense');
+  let date = new Date()
+  const [month, setMonth] = useState(date.getMonth() + 1)
+  const [year, setYear] = useState(date.getFullYear())
+  const [category, setCategory] = useState('')
+  console.log(category)
+  const [type, setType] = useState('expense')
 
-  const getIncomeDetail = useSelector(reportSelectors.getIncomeDetail);
-  const getExpenseDetail = useSelector(reportSelectors.getExpenseDetail);
-  const getTotalIncome = useSelector(reportSelectors.getTotalIncome);
-  const getTotalExpense = useSelector(reportSelectors.getTotalExpense);
-  const [totalIncome, setTotalIncome] = useState(getTotalIncome);
-  const [totalExpense, setTotalExpense] = useState(getTotalExpense);
-  
+  const getIncomeDetail = useSelector(reportSelectors.getIncomeDetail)
+  const getExpenseDetail = useSelector(reportSelectors.getExpenseDetail)
+  const getTotalIncome = useSelector(reportSelectors.getTotalIncome)
+  const getTotalExpense = useSelector(reportSelectors.getTotalExpense)
+  const [totalIncome, setTotalIncome] = useState(getTotalIncome)
+  const [totalExpense, setTotalExpense] = useState(getTotalExpense)
+
   useEffect(() => {
     setTotalIncome(getTotalIncome)
     setTotalExpense(getTotalExpense)
-  }, [getTotalIncome, getTotalExpense]);
+  }, [getTotalIncome, getTotalExpense])
 
   const onHandleClickRight = () => {
     if (month < 12) {
-      setMonth(prev => (prev += 1));
+      setMonth((prev) => (prev += 1))
     } else {
-      setMonth(1);
-      setYear(prev => (prev += 1));
+      setMonth(1)
+      setYear((prev) => (prev += 1))
     }
-  };
+  }
   const onHandleClickLeft = () => {
     if (month <= 1) {
-      setMonth(12);
-      setYear(prev => (prev -= 1));
+      setMonth(12)
+      setYear((prev) => (prev -= 1))
     } else {
-      setMonth(prev => (prev -= 1));
+      setMonth((prev) => (prev -= 1))
     }
-  };
+  }
   const onHandleChangeType = () => {
     if (type === 'expense') {
-      setType('income');
-      setCategory('');
+      setType('income')
+      setCategory('')
     }
     if (type === 'income') {
-      setType('expense');
-      setCategory('');
+      setType('expense')
+      setCategory('')
     }
-  };
-  const getCategory = e => {
-    setCategory(e.target.attributes.title.nodeValue);
-  };
+  }
 
   useEffect(() => {
-    dispatch(reportOperations.getExpenseDetail(`${year}-${month}`));
-    dispatch(reportOperations.getIncomeDetail(`${year}-${month}`))  
-  }, [dispatch, month, year, type]);
-  
+    dispatch(reportOperations.getExpenseDetail(`${year}-${month}`))
+    dispatch(reportOperations.getIncomeDetail(`${year}-${month}`))
+  }, [dispatch, month, year, type])
+
+  const handleCategoryClick = (category) => {
+    setCategory(category)
+  }
+
   return (
     <div className={s.container}>
-      {window.width > 767 ?
+      {window.width > 767 ? (
         <div className={s.navigation}>
           <ArrowGoBack />
           <Balance />
@@ -85,23 +94,42 @@ const ReportView = () => {
             month={month}
             year={year}
             onHandleClickRight={onHandleClickRight}
-            onHandleClickLeft={onHandleClickLeft} />
+            onHandleClickLeft={onHandleClickLeft}
+          />
         </div>
-        :
+      ) : (
         <div className={s.navigation}>
           <ArrowGoBack />
           <CurrentMonth
             month={month}
             year={year}
             onHandleClickRight={onHandleClickRight}
-            onHandleClickLeft={onHandleClickLeft}/>
+            onHandleClickLeft={onHandleClickLeft}
+          />
           <Balance />
-        </div>}
+        </div>
+      )}
       <div className={s.amoumt}>
-        <CurrentAmount totalIncome={totalIncome} totalExpense={ totalExpense }/>
+        <CurrentAmount totalIncome={totalIncome} totalExpense={totalExpense} />
       </div>
       <div className={s.amoumt}>
-        <OnHandleCategories type={type} onHandleChangeType={onHandleChangeType} />
+        <OnHandleCategories
+          type={type}
+          onHandleChangeType={onHandleChangeType}
+        />
+      </div>
+      <div>
+        {type === 'expense' ? (
+          <SummaryByCategory
+            transactions={getExpenseDetail}
+            onClick={handleCategoryClick}
+          />
+        ) : (
+          <SummaryByCategory
+            transactions={getIncomeDetail}
+            onClick={handleCategoryClick}
+          />
+        )}
       </div>
       {/* <ChartComponent /> */}
     </div>
