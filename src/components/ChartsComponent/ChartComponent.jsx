eb845ecc2b832a33e9e9b40783aec27455fdea11
AlexeyGrk <alexgmirko@gmail.com>
2021-11-16 22:54:19 +0200

    add chart component logic

--- src/components/ChartsComponent/ChartComponent.jsx
+++ src/components/ChartsComponent/ChartComponent.jsx
@@ -1,47 +1,24 @@
-import axios from "axios";
-import React, { useEffect, useState, useCallback } from "react";
-import {
-  fetchCurrentExpenses,
-  fetchCurrentIncome,
-} from "../../services/transactions-api";
+import React from "react";
+
 import { StatsChartContainer } from "../ChartsComponent/ChartComponent.styles";
 import StatsChartDesk from "./StatsChartDesk/StatsChartDesk";
 import StatsChartMobile from "./StatsChartMobile/StatsChartMobile";
 
 export const ChartComponent = ({
-  activeCategory = "Продукты",
-  income = false,
-  year = 2021,
-  month = 10,
+  getIncomeDetail,
+  getExpenseDetail,
+  type,
+  category = "Продукты",
 }) => {
-  const [transactions, setTransactions] = useState([]);
-  const [incomeTransactions, setIncomeTransactions] = useState([]);
-  console.log(transactions);
-  console.log(incomeTransactions);
-
-  useEffect(() => {
-    fetchCurrentExpenses(year, month).then((data) => {
-      if (transactions.length === 0) {
-        setTransactions(data);
-      }
-
-      fetchCurrentIncome(year, month).then((data) => {
-        if (incomeTransactions.length === 0) {
-          setIncomeTransactions(data);
-        }
-      });
-    });
-  }, [incomeTransactions, transactions]);
-
   return (
     <StatsChartContainer>
       <StatsChartDesk
-        transactions={income ? incomeTransactions : transactions}
-        activeCategory={activeCategory}
+        transactions={type === "expense" ? getExpenseDetail : getIncomeDetail}
+        activeCategory={category}
       />
       <StatsChartMobile
-        transactions={income ? incomeTransactions : transactions}
-        activeCategory={activeCategory}
+        transactions={type === "expense" ? getExpenseDetail : getIncomeDetail}
+        activeCategory={category}
       />
     </StatsChartContainer>
   );
