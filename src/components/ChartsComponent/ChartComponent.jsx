b8021e3dbca75d734e551d7fc1cbf797ee85370a
AlexeyGrk <alexgmirko@gmail.com>
2021-11-17 10:31:14 +0200

    add bugfix

--- src/components/ChartsComponent/ChartComponent.jsx
+++ src/components/ChartsComponent/ChartComponent.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-
+import { useSelector } from "react-redux";
+import reportSelectors from "../../redux/report/report-selectors";
 import { StatsChartContainer } from "../ChartsComponent/ChartComponent.styles";
 import StatsChartDesk from "./StatsChartDesk/StatsChartDesk";
 import StatsChartMobile from "./StatsChartMobile/StatsChartMobile";
+import Loader from "react-js-loader";
 
 export const ChartComponent = ({
   getIncomeDetail,
@@ -10,16 +12,37 @@ export const ChartComponent = ({
   type,
   category = "Продукты",
 }) => {
+  const IsLoading = useSelector(reportSelectors.getIsLoading);
+
   return (
     <StatsChartContainer>
-      <StatsChartDesk
-        transactions={type === "expense" ? getExpenseDetail : getIncomeDetail}
-        activeCategory={category}
-      />
-      <StatsChartMobile
-        transactions={type === "expense" ? getExpenseDetail : getIncomeDetail}
-        activeCategory={category}
-      />
+      {IsLoading ? (
+        <Loader
+          type="spinner-circle"
+          bgColor={"#ff751d"}
+          color={"#ff751d"}
+          size={60}
+        />
+      ) : (
+        <StatsChartDesk
+          transactions={type === "expense" ? getExpenseDetail : getIncomeDetail}
+          activeCategory={category}
+        />
+      )}
+
+      {IsLoading ? (
+        <Loader
+          type="spinner-circle"
+          bgColor={"#ff751d"}
+          color={"#ff751d"}
+          size={60}
+        />
+      ) : (
+        <StatsChartMobile
+          transactions={type === "expense" ? getExpenseDetail : getIncomeDetail}
+          activeCategory={category}
+        />
+      )}
     </StatsChartContainer>
   );
 };
