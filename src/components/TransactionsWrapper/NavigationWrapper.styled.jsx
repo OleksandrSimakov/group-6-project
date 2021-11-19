e32c5d797ba804de8e7cd84a9378e8967d4bf488
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-11-19 15:47:43 +0200

    Update NavigationWrapper.styled.jsx

--- src/components/TransactionsWrapper/NavigationWrapper.styled.jsx
+++ src/components/TransactionsWrapper/NavigationWrapper.styled.jsx
@@ -1,15 +1,15 @@
 import styled from '@emotion/styled/macro'
 
-// export const NavigationWrapper = styled.div`
-// @media screen and (min-width: 768px){
-//   margin: 0 auto 58px auto;
-//   display: flex;
-//   padding-top: 40px;
-//   justify-content: space-between;
-//   align-items: center;
-// }
-// @media screen and (min-width: 1280px) {
-//   margin: 0 auto 21px auto;
-//   justify-content: space-evenly;
-// }
-// `
+export const NavigationWrapper = styled.div`
+  @media screen and (min-width: 768px) {
+    margin: 0 auto 58px auto;
+    display: flex;
+    padding-top: 40px;
+    justify-content: space-between;
+    align-items: center;
+  }
+  @media screen and (min-width: 1280px) {
+    margin: 0 auto 21px auto;
+    justify-content: space-evenly;
+  }
+`
